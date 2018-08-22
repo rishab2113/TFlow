2319f9a57961d0265f7edbce8ceb9da15b3d4bb7
rishab2113 <rishab@bu.edu>
2018-08-22 22:02:56 +0530

    basic cnn intro

---
+++ CNNs/CNN_basic.py
@@ -0,0 +1,14 @@
+# Tensors are N-Dimensional Arrays (Scalar, Vector, Matrix, Tensor)
+# Tensors make it convenient to feed in sets of images into the model (Images, Height, Width, Color)
+
+# Convolutions have a major advantage for Image Processing, as the pixels next to each other are more correlated for image detection
+# Each CNN layer looks at an increasingly larger part of the image
+
+# Padding is essential for edge cases, where the convolution operation may not be possible due to lack of pixels
+
+# 1D vs 2D Convolution
+# Stride - Number of units at a time
+
+# Pooling Layers subsample the image, reducing memory use and computer load
+
+# Dropout can be thought of as a form of regularization ro prevent overfitting
