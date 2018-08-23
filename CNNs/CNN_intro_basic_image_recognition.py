c59621d4bda8c3c79161cde25a19017a2b6489f0
rishab2113 <rishab@bu.edu>
2018-08-23 10:04:45 +0530

    basic cnn + code formatting

--- CNNs/CNN_intro_basic_image_recognition.py
+++ CNNs/CNN_intro_basic_image_recognition.py
@@ -40,21 +40,22 @@ mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # Placeholders
 
-x = tf.placeholder(tf.float32, shape=[None,784])
+x = tf.placeholder(tf.float32, shape=[None, 784])
 
 # Variables
 
-W = tf.Variable(tf.zeros([784,10]))
+W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
 # Create Graph Operations
 
-y = tf.matmul(x,W) + b
+y = tf.matmul(x, W) + b
 
 # Loss function
 
-y_true = tf.placeholder(tf.float32,[None,10])
-cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y))
+y_true = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y))
 
 # Optimizer
 
@@ -69,10 +70,11 @@ with tf.Session() as sess:
     sess.run(init)
     for step in range(1000):
         batch_x, batch_y = mnist.train.next_batch(100)
-        sess.run(train, feed_dict={x:batch_x, y_true:batch_y})
+        sess.run(train, feed_dict={x: batch_x, y_true: batch_y})
 
-    correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_true,1))
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_true, 1))
 
-    acc = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    print(sess.run(acc, feed_dict={x:mnist.test.images, y_true: mnist.test.labels}))
+    print(sess.run(acc, feed_dict={
+          x: mnist.test.images, y_true: mnist.test.labels}))
